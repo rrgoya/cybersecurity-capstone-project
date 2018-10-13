d3b8079a5e2cf0bc6bbcecf1e70ecfc8a4d01710
Rodolfo Goya <rgoya@uol.com.br>
2018-10-13 02:52:13 -0300

    My simple index.php

--- php-getting-started-master/web/index.php
+++ php-getting-started-master/web/index.php
@@ -2,24 +2,4 @@
 
 require('../vendor/autoload.php');
 
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->run();
+echo "Alo mundo";
